bd3f7beed31157c3cab0227c3557d5a5b93d8d5d
Skowoo <skowoo@tlen.pl>
2024-12-01 13:48:26 +0100

    New base code uploaded

---
+++ Cwiczenia5/01.spa.api.options/01.porting.v-bind.ex/README.md
@@ -0,0 +1,37 @@
+# 01.porting.v-bind.ex
+
+This project demonstrates simply porting in-browser v-bind excercise progress bar component into SPA component declared with options API. Primary commit demonstrates porting component "logic" to root component. Next commits demonstrate components reusability (including defining props for components).
+
+About vue components (in general):
+https://vuejs.org/guide/essentials/component-basics.html
+
+About vue component props declaration:
+https://vuejs.org/guide/components/props
+
+## Recommended IDE Setup
+
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
+
+### Lint with [ESLint](https://eslint.org/)
+
+```sh
+npm run lint
+```
---
+++ Cwiczenia5/01.spa.api.options/01.porting.v-bind.ex/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <!-- when embeding components here are possible syntax alternatives:  -->
+  <!-- lower case with hypens explicit closing tag: RECOMMENED for both SPA nad in-broser modes  -->
+  <!-- component props binding is demonstrated foe barStyle and initialValue properties (Notice prop name as an attribute with lower case words splitted with hypens) -->
+  <progress-bar
+    class="wrapper"
+    style="width: 100%"
+    :bar-style="{ height: '20px' }"
+    :initial-value="10"
+  ></progress-bar>
+  <!-- lower case with hypens self closed tag - can be used in SPA but might not work in-browser -->
+  <progress-bar class="wrapper" style="width: 75%" :initial-value="20" />
+  <!-- pascal case with self closed tag - shortcut for SPA only -->
+  <ProgressBar
+    class="wrapper"
+    style="width: 50%"
+    :bar-style="{ height: '75px' }"
+    :initial-value="50"
+  />
+  <!-- pascal case with explict closing tag - SPA only -->
+  <ProgressBar
+    class="wrapper"
+    style="width: 25%"
+    :bar-style="{ height: '100px' }"
+  ></ProgressBar>
+  <!-- Plase notice style definition on components: because this componet has single root element (div) styles will be applied to inner component element. -->
+</template>
+
+<script>
+// component is declared in separate file and imported here
+import ProgressBar from './components/ProgressBar.vue'
+
+export default {
+  // for options api imported components have to be explicity declared inside options so that can exposed and used inside the template.
+  components: {
+    ProgressBar,
+  },
+}
+</script>
+
+<style scoped>
+.wrapper {
+  padding: 1rem;
+}
+</style>
